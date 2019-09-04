69d48be28494a77fde065bdec6eabc1818f40d19
Dr Great Wonder <51544476+drgreatwonder@users.noreply.github.com>
2019-09-04 13:10:01 +0100

    Add files via upload

---
+++ team-electron/app.js
@@ -0,0 +1,36 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+init();
+
+function init() {
+	scores = [0,0];
+	roundScore = 0;
+	activePlayer = 0;
+	gamePlaying = true;
+	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.querySelector('.player-0-rolled-1').style.visibility = 'hidden';
+	document.querySelector('.player-1-rolled-1').style.visibility = 'hidden';
+	
+	document.querySelector('#name-0').textContent = 'Player 1';
+	document.querySelector('#name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.add('active-0');
+	document.querySelector('.player-0-panel').classList.remove('winner-0');
+	document.querySelector('.player-1-panel').classList.remove('winner-1');
+
+}
+
+
